594b6dc7abff120a28a9b52427f310ea0589b0ee
Kellyfiocchi <fiocchi1989_faculdade@outlook.com>
2023-10-16 14:06:12 -0300

    update

---
+++ Models/Pessoa.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploExplorando.Models
+{
+    public class Pessoa
+    {
+
+        public Pessoa()
+        {
+            
+        }
+
+        public Pessoa(string nome, string sobrenome)
+        {
+           Nome = nome;
+           Sobrenome = sobrenome; 
+        }
+        
+        private string _nome;
+       // private int _idade;
+        public required string Nome 
+        {   
+            
+            get  => _nome.ToUpper();
+
+
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("Nome não pode ser vazio");
+                }
+
+                _nome = value;
+
+            }
+
+        }
+
+        public required string Sobrenome { get; set; }
+
+        public string GetNomeCompleto()
+        {
+            return $"{Nome} {Sobrenome}".ToUpper();
+        }
+
+        // public required int Idade 
+        // { 
+        //     get => _idade;
+
+
+        //     set
+        //     {
+        //         if(value < 0)
+        //         {
+        //             throw new ArgumentException("Idade não pode ser menor que zero");
+        //         }
+        //         _idade = value;
+        //     } 
+        //  }
+
+        // public void Apresentar()
+        // {
+        //     Console.WriteLine($"Nome:  {GetNomeCompleto()}, Idade: {Idade}");
+        // }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using ExemploExplorando.Models;
+
+
+Pessoa p1 = new Pessoa()
+{
+    Nome = "Kelly",
+    Sobrenome = "Fiochi"
+};
+
+Pessoa p2 = new Pessoa()
+{
+    Nome = "João",
+    Sobrenome = "Silva"
+};
+
+Curso cursoIngles = new Curso()
+{
+    Nome = "Inglês",
+    Alunos = new List<Pessoa>()
+};
+
+cursoIngles.AdicionarAluno(p1);
+cursoIngles.AdicionarAluno(p2);
+cursoIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa()
+// {
+//     Nome = "Kelly",
+//     Idade = 34,
+//     Sobrenome = "Fiochi"
+// };
+//   p1.Apresentar();
